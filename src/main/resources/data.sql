d183b9c6a91b65751fad612f43c288d80ba2c88d
dmitr <fly2sky@ya.ru>
2025-07-26 14:50:45 +0300

    задание 5 - добавлена миграция liquibase

--- src/main/resources/data.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO public.users
-(id, username)
-VALUES(nextval('users_id_seq'::regclass), concat('testUser', now()));
-INSERT INTO public.users
-(id, username)
-VALUES(nextval('users_id_seq'::regclass), concat('testUser', now()));
-INSERT INTO public.users
-(id, username)
-VALUES(nextval('users_id_seq'::regclass), concat('testUser', now()));
